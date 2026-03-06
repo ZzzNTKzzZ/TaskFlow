6816a3f5968a2b14409a0d46d7148b455805faed
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-06 21:53:21 +0700

    feat: add logout and refresh token

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -2,11 +2,61 @@ import type { Request, Response } from "express";
 import { AuthService } from "../services/auth.service.js";
 
 export class AuthController {
-    static async register(req: Request, res: Response) {
-        const { name, email, password } = req.body
+  static async register(req: Request, res: Response) {
+    const { name, email, password } = req.body;
 
-        const user = await AuthService.register(name, email, password)
+    const { user, accessToken, refreshToken } = await AuthService.register(
+      name,
+      email,
+      password,
+    );
 
-        res.send(201).json(user)
-    }
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 15 * 60 * 1000,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.send(201).json(user);
+  }
+
+  static async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const { user, accessToken, refreshToken } = await AuthService.login(
+      email,
+      password,
+    );
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 15 * 60 * 1000,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.send(201).json(user);
+  }
+
+  static async logout(req: Request, res: Response) {
+    const logout = await AuthService.logout(req.cookies.refreshToken)
+
+    res.clearCookie("refreshToken")
+    res.clearCookie("accessToken")
+
+    return res.send(201).json(logout.message)
+  }
 }
--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,38 +1,127 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import { prisma } from "../lib/prisma.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { AppError } from "../utils/appError.js";
+import { generateAccessToken, generateRefreshToken } from "../utils/token.js";
 export class AuthService {
-    static async register(name: string, email: string, password: string) {
-        const exitstingUser = await prisma.user.findUnique({
-            where: { email }
-        })
+  static async register(name: string, email: string, password: string) {
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
 
-        if(exitstingUser)  throw new AppError("Email already exists", 400)
+    if (existingUser) throw new AppError("Email already exists", 400);
 
-        const hashedPassword = await bcrypt.hash(password, 10)
-        
-        const user = await prisma.user.create({
-            data: {
-                name,
-                email,
-                password: hashedPassword,
-            }
-        })
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const token = jwt.sign(
-            {userId: user.id},
-            process.env.JWT_SECRET as string,
-            {expiresIn: "7d"}
-        )
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
 
-        return {
-            user: {
-                id: user.id,
-                name: user.name,
-                email: user.email
-            },
-            token
-        }
+    const accessToken = generateAccessToken(user.id);
+    const refreshToken = generateRefreshToken(user.id);
+
+    await prisma.refreshToken.create({
+      data: {
+        token: refreshToken,
+        userId: user.id,
+        expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      },
+    });
+
+    return {
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  static async login(email: string, password: string) {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) throw new AppError("Invalid email or password", 401);
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) throw new AppError("Invalid email or password", 401);
+
+    const accessToken = generateAccessToken(user.id);
+    const refreshToken = generateRefreshToken(user.id);
+
+    await prisma.refreshToken.create({
+      data: {
+        token: refreshToken,
+        userId: user.id,
+        expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      },
+    });
+
+    return {
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  static async logout(refreshToken: string) {
+    await prisma.refreshToken.delete({
+      where: { token: refreshToken },
+    });
+
+    return { message: "Logged out successfully" }
+  }
+
+  static async refreshToken(refreshToken: string) {
+    if (!refreshToken) throw new AppError("No refresh token provided", 401);
+
+    let decode: any;
+
+    try {
+      decode = jwt.verify(
+        refreshToken,
+        process.env.JWT_REFRESH_SECRET as string,
+      );
+    } catch (error) {
+      throw new AppError("Refresh token expired or invalid", 401);
     }
+
+    const storedToken = await prisma.refreshToken.findUnique({
+      where: { token: refreshToken },
+    });
+
+    if (!storedToken || storedToken.expiresAt < new Date())
+        throw new AppError("Refresh token not found", 401);
+    await prisma.refreshToken.delete({
+        where: { token: refreshToken}
+    })
+
+    const newAccessToken = generateAccessToken(decode.userId);
+    const newRefreshToken = generateRefreshToken(decode.userId);
+
+    await prisma.refreshToken.create({
+        data: {
+            userId: decode.userId,
+            token: newRefreshToken,
+            expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+
+        }
+    })
+    return {
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
+    };
+  }
 }
