4a83b44c37cfe47c8600a4b1a9a6b0afac329d89
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-07 22:50:39 +0700

    feat: authencation middleware

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,21 +1,23 @@
-import jwt from "jsonwebtoken"
-import type { Request, Response, NextFunction } from "express"
-import { AppError } from "../utils/appError.js"
-const authMiddleware = () => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const auth = req.headers.authorization
-            const [scheme, token] = auth?.split(" ") ?? [];
+import jwt from "jsonwebtoken";
+import type { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/appError.js";
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const auth = req.headers.authorization;
+    const [scheme, token] = auth?.split(" ") ?? [];
 
-            if(!token || scheme !== "Bearer") throw new AppError( "Unauthorized: No token provided", 401)
-            
+    if (!token || scheme !== "Bearer")
+      throw new AppError("Unauthorized: No token provided", 401);
 
-            const decode = jwt.verify(token, process.env.JWT_SECRET!);
+    const decode = jwt.verify(token, process.env.JWT_SECRET!);
 
-            (req as any).user = decode
-            next()
-        } catch (error) {
-            throw new AppError("Unauthorized: Invalid token", 401)
-        }
-    }
-}
+    (req as any).user = decode;
+    next();
+  } catch (error) {
+    next(new AppError("Unauthorized: Invalid token", 401));
+  }
+};
