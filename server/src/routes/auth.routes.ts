ae51628de37d5d9ebc03e931ba3b5cae8fb6cf8a
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-08 17:27:12 +0700

    feat(auth): add validation middleware to register and login routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { AuthController } from "../controllers/auth.controller.js";
 import { validateMiddleware } from "../middleware/validate.middleware.js";
-import { registerSchema } from "../validators/auth.schema.js";
+import { loginSchema, registerSchema } from "../validators/auth.schema.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const authRoutes = Router();
@@ -12,7 +12,17 @@ authRoutes.post(
   validateMiddleware(registerSchema),
   asyncHandler(AuthController.register),
 );
-authRoutes.post("/login", asyncHandler(AuthController.login));
-authRoutes.post("/logout", authMiddleware, asyncHandler(AuthController.logout))
-authRoutes.post("/refresh", authMiddleware, asyncHandler(AuthController.refresh))
+authRoutes.post(
+  "/login",
+  validateMiddleware(loginSchema),
+  asyncHandler(AuthController.login),
+);
+authRoutes.post("/logout", authMiddleware, asyncHandler(AuthController.logout));
+authRoutes.post(
+  "/refresh`",
+  authMiddleware,
+  asyncHandler(AuthController.refresh),
+);
+
+authRoutes.get("/me", authMiddleware, asyncHandler(AuthController.getMe))
 export default authRoutes;
