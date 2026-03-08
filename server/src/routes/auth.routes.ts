69e809f296432066e3f8f8579fd0a9a50d5b325b
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-08 14:43:39 +0700

    feat: add logout and refresh in routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { AuthController } from "../controllers/auth.controller.js";
+import { validateMiddleware } from "../middleware/validate.middleware.js";
+import { registerSchema } from "../validators/auth.schema.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
-const authRoutes = Router()
+const authRoutes = Router();
 
-authRoutes.post("/register", asyncHandler(AuthController.register))
-authRoutes.post("/login", asyncHandler(AuthController.login))
-
-export default authRoutes
+authRoutes.post(
+  "/register",
+  validateMiddleware(registerSchema),
+  asyncHandler(AuthController.register),
+);
+authRoutes.post("/login", asyncHandler(AuthController.login));
+authRoutes.post("/logout", authMiddleware, asyncHandler(AuthController.logout))
+authRoutes.post("/refresh", authMiddleware, asyncHandler(AuthController.refresh))
+export default authRoutes;
