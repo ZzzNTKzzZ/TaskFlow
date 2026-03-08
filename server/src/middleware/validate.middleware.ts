bb0cb1d5688d0614f11d22f2f1dfe9290c1bdb8e
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-08 13:28:06 +0700

    feat: create validate middle ware

---
+++ server/src/middleware/validate.middleware.ts
@@ -0,0 +1,13 @@
+import { success, type z, type ZodRawShape } from "zod";
+import type { Response, Request, NextFunction } from "express";
+import { AppError } from "../utils/appError.js";
+export const validateMiddleware =
+  <T extends ZodRawShape>(schema: z.ZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+
+    if(!success) return res.status(400).json(result.error)
+
+    req.body = result.data
+    next()
+  };
---
+++ server/src/validators/auth.schema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod"
+
+export const registerSchema  = z.object({
+    name: z.string().min(3),
+    email: z.email(),
+    password: z.string().min(6)
+})
+
