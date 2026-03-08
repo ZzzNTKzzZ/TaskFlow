f3ea071b042d1d4059328115b51d5c09bfd7fc89
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-08 17:27:55 +0700

    feat: add workspace route

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,5 +1,8 @@
 import type { Express } from "express"
 import authRoutes from "./auth.routes.js"
+import { authMiddleware } from "../middleware/auth.middleware.js"
+import workspaceRoutes from "./workspace.routes.js"
 export const routes = (app: Express) => {
     app.use("/auth", authRoutes)
+    app.use("/workspaces", authMiddleware, workspaceRoutes)
 }
