04e2011c52f97237f491256893d50a0341416f05
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-10 21:15:31 +0700

    feat: add permissions middleware

---
+++ server/src/middleware/permissions.middleware.ts
@@ -0,0 +1,20 @@
+import type { NextFunction, Request, Response } from "express";
+import permissions from "../permissions/permissions.map.js";
+import type { PermissionAction } from "../permissions/permissions.map.js";
+import { AppError } from "../utils/appError.js";
+
+export const permissionMiddleware = (action: PermissionAction) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const role = req.workspaceRole;
+
+    if (!role) {
+      return next(new AppError("Workspace role not found", 404));
+    }
+
+    if (!permissions[action].includes(role)) {
+      return next(new Error("Forbidden"));
+    }
+
+    next();
+  };
+};
