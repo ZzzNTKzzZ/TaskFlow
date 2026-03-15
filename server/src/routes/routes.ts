90ef50be8a12ec73d3a11f24bc4a3d76e386bb94
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-16 01:00:27 +0700

    refactor(router): update main routes and board schemas for module-based architecture

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,8 +1,11 @@
-import type { Express } from "express"
-import authRoutes from "./auth.routes.js"
-import { authMiddleware } from "../middleware/auth.middleware.js"
-import workspaceRoutes from "./workspace.routes.js"
+import type { Express } from "express";
+import authRoutes from "../modules/Auth/auth.routes.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+import workspaceRoutes from "../modules/Workspace/workspace.routes.js";
+import boardRoutes from "../modules/Board/board.routes.js";
+import { boardAccessMiddleware } from "../middleware/boardAccess.middleware.js";
 export const routes = (app: Express) => {
-    app.use("/auth", authRoutes)
-    app.use("/workspaces", authMiddleware, workspaceRoutes)
-}
+  app.use("/auth", authRoutes);
+  app.use("/workspaces", authMiddleware, workspaceRoutes);
+  app.use("/boards", authMiddleware, boardAccessMiddleware, boardRoutes);
+};
